f59cec59ec5c03fcb41d2dd0daeaadb5437f0ba5
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2024-09-23 19:43:34 -0400

    Update cbc-news-streams.js

--- src/cbc-news-streams.js
+++ src/cbc-news-streams.js
@@ -1,3 +1,4 @@
+ 
 /**!
  * @license CBC-News-Streams.js - A JavaScript library for fetching free live TV stream URLs from CBC.ca / CBC News
  * VERSION: 1.0.0
@@ -7,6 +8,8 @@
  */
 export function CBC_News_Streams() {
 
+     const LIST_ELEMENT = "2415871718";
+  
     if (typeof node !== 'undefined' || typeof process === 'object') {
         try {
             let fetch = require('node-fetch');
@@ -20,8 +23,12 @@ export function CBC_News_Streams() {
 
     async function getAllChannels() {
         try {
-            let result = await fetchURL("url", "https://tpfeed.cbc.ca/f/ExhSPC/t_t3UKJR6MAT?pretty=true&sort=pubDate%7Cdesc");
-            return await fetchAllChannelURLs(result)
+            let result = await fetchURL("url", "https://services.radio-canada.ca/ott/catalog/v2/gem/home?device=web");
+            result = result.lineups.results
+          
+           const items = result.find((result) => result.key === LIST_ELEMENT)?.items || [];
+          
+            return await fetchAllChannelURLs(items)
         } catch (err) {
             throw err
         }
@@ -35,8 +42,9 @@ export function CBC_News_Streams() {
             }
             // Uppercase the first letter.
             channel_title = channel_title.charAt(0).toUpperCase() + channel_title.slice(1);
-            let result = await fetchURL("url", "https://tpfeed.cbc.ca/f/ExhSPC/t_t3UKJR6MAT?pretty=true&sort=pubDate%7Cdesc");
-            let channels = result.entries
+            let result = await fetchURL("url", "https://services.radio-canada.ca/ott/catalog/v2/gem/home?device=web");
+            let channels = result.lineups.results
+            channels =  channels.find((result) => result.key === LIST_ELEMENT)?.items || []; 
             let foundResults = false
             for (const channel in channels) {
                 if (channel_title === channels[channel].title) {
@@ -45,7 +53,7 @@ export function CBC_News_Streams() {
                         channel_title: channels[channel].title,
                         description: channels[channel].description,
                         channel_image: channels[channel].cbc$staticImage,
-                        stream_url: await getStreamData(channels[channel].content[0].url)
+                        stream_url: await getStreamData(channels[channel].idMedia)
                     }
                     return results
                 }
@@ -80,50 +88,31 @@ export function CBC_News_Streams() {
 
     // Core function to fetch all channels
     async function fetchAllChannelURLs(json) {
-        let results = []
-        for (const channel in json) {
-            for (const id in json[channel]) {
-                if (json[channel][id].id) {
-                    results.push({
-                        channel_title: json[channel][id].title,
-                        description: json[channel][id].description,
-                        channel_image: json[channel][id].cbc$staticImage,
-                        stream_url: await getStreamData(json[channel][id].content[0].url)
-                    })
-                }
-            }
-        }
+        const results = await Promise.all(json.map(async item => ({
+    channel_title: item.title,
+    description: item.description,
+    channel_image: item.images.card,
+    stream_url: await getStreamData(item.idMedia)
+})));
+
         return results
     }
 
 
-    async function getStreamData(url) {
+    async function getStreamData(id) {
         try {
-            let result = await fetchURL("text", url);
-            return parseXML(result)
+            let result = await fetchURL("url", `https://corsproxy.io/?https://services.radio-canada.ca/media/validation/v2/?appCode=medianetlive&connectionType=hd&deviceType=ipad&idMedia=${id}&multibitrate=true&output=json&tech=hls&manifestType=desktop`);
+            return  result
         } catch (err) {
             throw err
             return
         }
     }
-
-    // Core function to parse XML files from CBC
-    function parseXML(file) {
-        let xmlDoc;
-        let text = file
-        xmlDoc = new DOMParser().parseFromString(text, 'text/xml');
-        let videoTag = xmlDoc.querySelector("video").outerHTML
-        const regex = /src="[^"]*"/gm;
-        const found = videoTag.match(regex);
-        // some hacky work-around to parse source
-        let parsedResults = found[0].replace('src="', '');
-        parsedResults = parsedResults.replace('"', '');
-        // return the parsed stream SRC 
-        return parsedResults
-    } 
+ 
 
     return {
         getChannel: getChannel,
         getAllChannels: getAllChannels
     }
 }
+     console.log(await CBC_News_Streams().getChannel("Toronto")) 
